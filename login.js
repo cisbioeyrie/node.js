6ed92e129cf0b8508d951fc615a53a4ac0467ca1
cisbioeyrie <65909365+cisbioeyrie@users.noreply.github.com>
2020-05-31 20:41:52 +0800

    Add files via upload

--- login.js
+++ login.js
@@ -3,8 +3,7 @@ var express = require('express');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 const path = require('path');
-const async = require('async');
-
+const popup = require('node-popup');
 //const { body,validationResult } = require('express-validator');
 
 var con = mysql.createConnection({
@@ -17,6 +16,7 @@ var con = mysql.createConnection({
 var app = express();
 app.use(session({
 	secret: 'secret',
+    cookie: { maxAge: 6000000 },
 	resave: false,
 	saveUninitialized: false
 }));
@@ -60,8 +60,7 @@ app.post('/auth', function(request, response) {
 				request.session.tid = userid;
 				request.session.tname = result[0].t_name;
 				request.session.cid = result[0].course_id;
-				 console.log(result[0].t_name+' '+result[0].course_id);
-				 response.render('/teacher');
+				response.render('teacher',{tname:result[0].t_name,cid:result[0].course_id});
 			} else 		
 				if(!response.headersSent){
 				response.send('<a href="/">Back to login</a>');
@@ -73,13 +72,44 @@ app.post('/auth', function(request, response) {
 	}
 });
 
-/*app.get('/student', function(request, response) {
+app.get('/student', function(request, response) {
 	if (request.session.loggedin) {
 		response.send('Welcome back, ' + request.session.userid + '!');
 	} else {
 		response.send('Please login to view this page!');
 	}
 	response.end();
-});*/
+});
+
+app.get('/all_attendance', function(request, response) {
+    con.query('select week from attendance where course_id=? and student_id=\'s2020001\'', [request.session.cid], function(err, result, fields){
+        con.query('select * from attendance where course_id=?', [request.session.cid], function(err, result1, fields){
+       	response.render('all_attendance',{all_attendance_record:result1 ,all_attendance_week:result});
+        response.end(); 
+        });
+    });
+});
+
+app.post('/seturl', function(request, response) {
+    var cid=request.session.cid;
+    var week=request.body.week;
+    con.query('update courses set URL=?,week=? where id=?;', [request.body.url,week,cid], function(err, result, fields){});
+    con.query('select id from student where courses_id like \'%?%\'',parseInt(cid), function(err, result, fields){
+        if (err) throw err;
+        for(var i in result){                     
+            con.query('insert into attendance values(?,?,\'F\',?)',[cid,result[i].id,week], function(err1, result1, fields1){
+                if (err1) throw err1;
+            });
+        }
+        console.log('all good!');
+    });
+});     
+
+app.post('/change_attendance', function(request, response) {
+    if(request.body.s_id)
+        con.query('update attendance set status = ? where week =? and student_id =?;',[request.body.status,request.body.week,request.body.s_id], function(err, result, fields){});
+    else
+        con.query('update attendance set status = ? where week =?;',[request.body.status,request.body.week], function(err, result, fields){});
+});
 
 app.listen(3000);
